c66d92adb83789200d56956338fcb8eae26437e9
Giovane Aguiar <giovane.aguiar@ice.ufjf.br>
2023-09-17 18:45:58 -0300

    início dos testes

---
+++ src/test/java/DocumentoFactoryTest.java
@@ -0,0 +1,25 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class DocumentoFactoryTest {
+
+    @Test
+    public void testDocumentoDeTexto() {
+        DocumentoFactory textoFactory = new DocumentoDeTextoFactory();
+        Documento documentoDeTexto = textoFactory.criarDocumento();
+
+        assertEquals("Abrindo um documento de texto.", documentoDeTexto.abrir());
+        assertEquals("Salvando um documento de texto.", documentoDeTexto.salvar());
+        assertEquals("Fechando um documento de texto.", documentoDeTexto.fechar());
+    }
+
+    @Test
+    public void testDocumentoDePlanilha() {
+        DocumentoFactory planilhaFactory = new DocumentoDePlanilhaFactory();
+        Documento documentoDePlanilha = planilhaFactory.criarDocumento();
+
+        assertEquals("Abrindo um documento de planilha.", documentoDePlanilha.abrir());
+        assertEquals("Salvando um documento de planilha.", documentoDePlanilha.salvar());
+        assertEquals("Fechando um documento de planilha.", documentoDePlanilha.fechar());
+    }
+}
